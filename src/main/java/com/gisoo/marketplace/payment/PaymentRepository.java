429a91d2234086490d3571f69e4cc2d087789306
gisoogholizade-ux <gholizadegisoo@gmail.com>
2026-08-27 07:51:51 +0330

    Add payment repository

---
+++ src/main/java/com/gisoo/marketplace/payment/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.gisoo.marketplace.payment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findAllByOrderIdOrderByCreatedAtDesc(Long orderId);
+}
