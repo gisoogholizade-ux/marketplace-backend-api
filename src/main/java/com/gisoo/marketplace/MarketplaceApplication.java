733e642acc1833a907b37fb23e0fa81c0d8969ca
gisoogholizade-ux <gholizadegisoo@gmail.com>
2026-08-28 09:32:35 +0330

    Use stable paginated API serialization

--- src/main/java/com/gisoo/marketplace/MarketplaceApplication.java
+++ src/main/java/com/gisoo/marketplace/MarketplaceApplication.java
@@ -2,8 +2,10 @@ package com.gisoo.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class MarketplaceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceApplication.class, args);
